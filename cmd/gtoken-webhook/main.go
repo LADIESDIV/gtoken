cd4edc418ca0de712e832c798973ce0383844950
Laetitia Diverchy <laetitia.diverchy.partner@decathlon.com>
2023-07-17 07:48:50 +0200

    feat: add env variable

--- cmd/gtoken-webhook/main.go
+++ cmd/gtoken-webhook/main.go
@@ -450,7 +450,7 @@ func main() {
 				cli.StringFlag{
 					Name:  "image",
 					Usage: "Docker image with secrets-init utility on board",
-					Value: fmt.Sprintf("%+v", os.Setenv("IMAGE_DOCKER", "dockerld/gtoken")),
+					EnvVars: []string{"IMAGE_DOCKER"},
 				},
 				cli.StringFlag{
 					Name:  "pull-policy",
